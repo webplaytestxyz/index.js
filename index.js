cebccbe59ed68a9df5999c1d2139ce17eff20941
webplaytestxyz <123665746+webplaytestxyz@users.noreply.github.com>
2023-01-26 20:19:51 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+const hostname = '127.0.0.1';
+const port = 3000;
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+<html>
+  <head>
+    <title>My Webpage</title>
+    <style>
+      /* Set styles for the header, content, and sidebar */
+      #header {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 15%;
+        background-color: #ddd;
+      }
+      
+      #container {
+        position: absolute;
+        top: 15%;
+        left: 0;
+        width: 100%;
+        height: 85%;
+        display: flex;
+      }
+      
+      #content {
+        flex: 0.75;
+        background-color: #eee;
+      }
+      
+      #sidebar {
+        flex: 0.25;
+        background-color: #ccc;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="header">
+      <h1>Upper Menu</h1>
+    </div>
+    <div id="container">
+      <div id="content">
+        <h2>Content Area</h2>
+        <p>This is where the main content of the webpage will go.</p>
+      </div>
+      <div id="sidebar">
+        <h2>Sidebar</h2>
+        <p>This is where the sidebar content will go.</p>
+      </div>
+    </div>
+  </body>
+</html>
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+  `);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
